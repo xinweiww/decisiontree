be1661a14e3f46ae3b903cba37d8672d53bedc1e
xinweiww <149534449+xinweiww@users.noreply.github.com>
2023-11-16 23:51:42 -0500

    2351

--- tests/testthat/test-myrpart.R
+++ tests/testthat/test-myrpart.R
@@ -1,5 +1,3 @@
-expect_true(exists("model", where = globalenv()))
-
 test_that("model is created", {
   data <- data.frame(feature1 = runif(10),feature2 = runif(10),target = sample(c("A", "B"), 10, replace = TRUE))
   model <- myrpart(data, "target", max_depth = 3)
